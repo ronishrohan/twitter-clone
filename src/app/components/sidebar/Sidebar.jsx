b975e3c20a9fbeafe78fed64ecd7e8ed6ef71156
Roni <64074505+RonishRohan@users.noreply.github.com>
2024-03-13 14:29:33 +0530

    changed color scheme to dark and minor fixes

--- src/app/components/sidebar/Sidebar.jsx
+++ src/app/components/sidebar/Sidebar.jsx
@@ -12,24 +12,24 @@ function Sidebar() {
         href="/home"
         className="text-4xl  font-medium w-full items-center hidden lg:flex lg:justify-normal justify-center border-b border-transparent p-4 hover:border-grays-200 hover:bg-grays-100 transition-all"
       >
-        <span>chirp</span>
+        <span>waffle</span>
       </Link>
       <div className="flex flex-col">
-        <NavLink href="/home" icon={icons.home}>
+        <NavLink href="/home" title="Home" icon={icons.home}>
           Home
         </NavLink>
-        <NavLink href="/explore" icon={icons.search}>
+        <NavLink href="/explore" title="Explore" icon={icons.search}>
           Explore
         </NavLink>
-        <NavLink href="/messages" icon={icons.messages}>
+        <NavLink href="/messages" title="Messages" icon={icons.messages}>
           Messages
         </NavLink>
-        <NavLink href="/profile" icon={icons.profile}>
+        <NavLink href="/profile" title="Profile" icon={icons.profile}>
           Profile
         </NavLink>
       </div>
       <div className="flex flex-col mt-auto">
-        <button className="filter hover:brightness-110 transition-all  h-14 bg-gradient-to-r from-accent-800 to-blue-600 m-4  text-white font-bold rounded-full text-lg">
+        <button title="Post" className="filter hover:brightness-110 transition-all  h-14 bg-gradient-to-r from-accent-800 to-blue-600 m-4  text-white font-bold rounded-full text-lg">
           <span className="hidden lg:block">Post</span>
           <span className="lg:hidden block">{icons.plus}</span>
         </button>
