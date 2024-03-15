cd35491126ef04a46e753b05a032b834c19f6c06
Ronish Rohah(linux) <ronish.rohan@gmail.com>
2024-03-15 21:07:12 +0530

    idk changed something

--- src/app/api/generate/route.js
+++ src/app/api/generate/route.js
@@ -3,15 +3,20 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 
 export async function POST(req){
-    const genAI = new GoogleGenerativeAI(process.env.GEMINI_SECRET)
-
-    const model = genAI.getGenerativeModel({model: "gemini-pro"})
-    const data = await req.json()
-
-    const result = await model.generateContent(data.query)
-    const response = await result.response;
-    const text = response.text();
-    console.log(text)
-    return Response.json({status: 201, response: text})
+    try {
+        const genAI = new GoogleGenerativeAI(process.env.GEMINI_SECRET)
     
+        const model = genAI.getGenerativeModel({model: "gemini-pro"})
+        const data = await req.json()
+    
+        const result = await model.generateContent(data.query)
+        const response = await result.response;
+        const text = response.text();
+        console.log(text)
+        return Response.json({status: 201, response: text})
+        
+    } catch (error) {
+        console.log(error)
+        return Response.json({status: 404})
+    }
 }
